ff8a325d29d822484b9a29758fa43ff782f47bc1
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-07-11 10:13:43 +0100

    update/db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
+// post_auth/config/db.js
+// 
 const connectDB = async () => {
+
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
