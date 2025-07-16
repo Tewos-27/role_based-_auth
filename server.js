44e0d3339882d7086767e7ce7dda02c17cbb57b1
Tewodros Shimels <tewodrosshimels54@gmail.com>
2025-07-16 12:54:00 +0100

    update/changes

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
-
+// Start the server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
